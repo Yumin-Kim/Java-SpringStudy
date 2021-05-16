f247feeb9b7f6bba1bcf838adc691358fafe0c59
Yumin-Kim <dbals0@naver.com>
2021-05-16 22:08:42 +0900

    First Commit [스프링 강의를 통해 간단한 개념 정리]

---
+++ src/test/java/startspring/hello_spring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package startspring.hello_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
